32459b6792274d1730d268bb28267d1b13ff6be3
BillJavanainen <william.javanainen@havocai.com>
2026-02-13 13:49:50 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -38,9 +38,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://small-block-8bd7.william-javanainen.workers.dev"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
